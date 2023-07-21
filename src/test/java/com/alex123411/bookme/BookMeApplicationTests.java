87f0e95db1e73ee12e441cbdede205697b539ff3
alex123411 <alextorgovskyh@gmail.com>
2023-07-20 21:34:58 -0500

    Initial commit

---
+++ src/test/java/com/alex123411/bookme/BookMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex123411.bookme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
