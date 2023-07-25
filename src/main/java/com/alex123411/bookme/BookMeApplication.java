203d49131ec5fea72bc6f88d6cb1a49e41136116
alex123411 <alextorgovskyh@gmail.com>
2023-07-25 03:32:47 +0200

    Security config added, so POST methods can go on without CSRF token. Added basic classes file for User, controller, repo, service.

--- src/main/java/com/alex123411/bookme/BookMeApplication.java
+++ src/main/java/com/alex123411/bookme/BookMeApplication.java
@@ -3,6 +3,11 @@ package com.alex123411.bookme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(
+//        scanBasePackageClasses = {
+//        // List of packages outside the "bookme" package, for example "com.alex123411.utils"
+//        }
+//)
 @SpringBootApplication
 public class BookMeApplication {
 
--- src/main/java/com/alex123411/bookme/controllers/UserController.java
+++ src/main/java/com/alex123411/bookme/controllers/UserController.java
@@ -1,18 +1,27 @@
 package com.alex123411.bookme.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alex123411.bookme.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("api/v1/user")
 public class UserController {
 
-    @PostMapping("/register")
-    public String register() {
+    private final UserService userService;
 
-        return "registered";
+    //If we have one constructor @Autowired is optional annotation.
+    @Autowired
+    // If we have different implementations of UserService (for example it is an interface)
+    // we must qualify the bean we want to use, for example
+    // @Qualifier("mysqlUserService")
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody String name) {
+        return userService.registerUser(name);
     }
 
     @PostMapping("/login")
