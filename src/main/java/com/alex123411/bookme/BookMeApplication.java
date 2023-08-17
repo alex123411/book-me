9b5b8903eb34d8bea1750a99e20f7725c2da0ee1
alex123411 <alextorgovskyh@gmail.com>
2023-08-16 20:59:25 -0500

    Default user on creation
    Manager and admin user profiles are created by default on application start.

--- src/main/java/com/alex123411/bookme/BookMeApplication.java
+++ src/main/java/com/alex123411/bookme/BookMeApplication.java
@@ -1,9 +1,14 @@
 package com.alex123411.bookme;
 
+import com.alex123411.bookme.auth.AuthenticationService;
+import com.alex123411.bookme.auth.RegisterRequest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import static com.alex123411.bookme.user.Role.ADMIN;
+import static com.alex123411.bookme.user.Role.MANAGER;
 
 import java.util.Arrays;
 
@@ -17,6 +22,33 @@ public class BookMeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookMeApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+            var manager = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("manager@mail.com")
+                    .password("password")
+                    .role(MANAGER)
+                    .build();
+            System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+        };
     }
 
 }
