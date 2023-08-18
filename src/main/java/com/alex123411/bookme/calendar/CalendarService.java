2f1bb6b74b4ccdfc8bdb9a0db9589a9ababcb2ec
alex123411 <alextorgovskyh@gmail.com>
2023-08-17 21:50:56 -0500

    added calendar service to calendar module

--- src/main/java/com/alex123411/bookme/calendar/CalendarService.java
+++ src/main/java/com/alex123411/bookme/calendar/CalendarService.java
@@ -1,4 +1,23 @@
 package com.alex123411.bookme.calendar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class CalendarService {
+
+    @Autowired
+    private CalendarRepository calendarRepository;
+
+    public List<Calendar> getAllCalendars() {
+        return calendarRepository.findAll();
+    }
+
+    public Calendar createCalendar(Calendar calendar) {
+        return calendarRepository.save(calendar);
+    }
+
+    // Other CRUD operations and business logic as needed
 }
+
